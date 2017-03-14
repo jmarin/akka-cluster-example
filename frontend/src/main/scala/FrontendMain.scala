287c43c323401bd1715c9ad3c12e614dcb1c1ac4
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-03-14 15:41:49 -0400

    Update configuration for docker

--- frontend/src/main/scala/FrontendMain.scala
+++ frontend/src/main/scala/FrontendMain.scala
@@ -6,9 +6,7 @@ import com.typesafe.config.ConfigFactory
 
 object FrontendMain {
   def main(args: Array[String]): Unit = {
-    val port = if (args.isEmpty) "0" else args(0)
-    val config = ConfigFactory.parseString(s"akka.remote.netty.tcp.port=$port")
-      .withFallback(ConfigFactory.parseString("akka.cluster.roles = [frontend]"))
+    val config = ConfigFactory.parseString("akka.cluster.roles = [frontend]")
       .withFallback(ConfigFactory.load())
     val actorSystemName = config.getString("cluster.name")
     val system = ActorSystem(actorSystemName, config)
