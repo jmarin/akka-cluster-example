f97e945e8c549fd08b4699c18203fa4e8b0e414a
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-03-15 16:13:29 -0400

    Change to service account

--- build.groovy
+++ build.groovy
@@ -8,20 +8,14 @@ node('platform-ops') {
         sh "${tool name: 'sbt 0.13.1', type: 'org.jvnet.hudson.plugins.SbtPluginBuilder$SbtInstallation'}/bin/sbt clean test assembly"
     }
 
-    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'dtr',
-                      usernameVariable: 'PLATFORM_USERNAME', passwordVariable: 'PLATFORM_PASSWORD']]) {
-        //available as an env variable, but will be masked if you try to print it out any which way
-        echo "${env.USERNAME}"
+    docker.withRegistry('https://dtr.cfpb.gov', 'svc_jenkins') {
+      stage('Docker Build') {
+          sh "git rev-parse HEAD > .git/commit-id"
+          def commit_id = readFile('.git/commit-id').trim()
+          println(commit_id)
 
-        docker.withRegistry('https://dtr.cfpb.gov', 'dtr') {
-            stage('Docker Build') {
-                sh "git rev-parse HEAD > .git/commit-id"
-                def commit_id = readFile('.git/commit-id').trim()
-                println(commit_id)
-
-                def seedImage = docker.build("dtr.cfpb.gov/akka-cluster-example-seed:${env.BUILD_TAG}", "--build-arg ./seed")
-                seedImage.push('latest')
-            }
-        }
+          def seedImage = docker.build("dtr.cfpb.gov/akka-cluster-example-seed:${env.BUILD_TAG}", "--build-arg ./seed")
+          seedImage.push('latest')
+      }
     }
 }
