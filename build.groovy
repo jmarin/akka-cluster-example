a82fc67285a8d076fd8de8e3965df38ca9800619
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-03-15 12:11:53 -0400

    Add sbt assembly

--- build.groovy
+++ build.groovy
@@ -5,6 +5,6 @@ node {
 
     stage('Build') {
         // assumes you have the sbt plugin installed and created an sbt installation named 'sbt-0.13.13'
-        sh "${tool name: 'sbt', type: 'org.jvnet.hudson.plugins.SbtPluginBuilder$SbtInstallation'}/bin/sbt compile test"
+        sh "${tool name: 'sbt', type: 'org.jvnet.hudson.plugins.SbtPluginBuilder$SbtInstallation'}/bin/sbt clean test assembly"
     }
 }
