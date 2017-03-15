8cb5fc20bf8ff51a0d4ec58e9f84040b6e4e1ddd
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-03-15 14:16:50 -0400

    Debug a bit

--- build.groovy
+++ build.groovy
@@ -14,8 +14,10 @@ node {
           //sh "cp frontend/target/scala-2.12/frontend.jar frontend/."
           //sh "cp backend/target/scala-2.12/backend.jar backend/."
 
-          def branch_name = ${env.BRANCH_NAME}.minus('origin/')
-          println branch_name
+
+          sh "git rev-parse HEAD > .git/commit-id"
+          def commit_id = readFile('.git/commit-id').trim()
+          println(commit_id)
 
           //def commit_id = readFile(".git/refs/heads/${branch_name}").trim()
           //def seedImage = docker.build("dtr.cfpb.gov/akka-cluster-example-seed:${commit_id}", "--build-arg PACKAGE_VERSION=${commit_id} ./seed")
