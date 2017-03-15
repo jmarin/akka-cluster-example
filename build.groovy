d95a21bcd40e4cc9aa0a45e313036f2a39ba2aa6
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-03-15 11:54:20 -0400

    Rewrite build.groovy

--- build.groovy
+++ build.groovy
@@ -1,24 +1,14 @@
-pipeline {
-    agent any
-
-    tools {
-        jdk 'jdk8'
-        org.jvnet.hudson.plugins.SbtPluginBuilder$SbtInstallation 'sbt'
+node {
+    stage('Git') {
+        git 'https://github.com/jmarin/akka-cluster-example.git'
     }
 
-    stages {
-        stage('Initialize') {
-            steps {
-                sh '''
-                  echo "PATH = $PATH"
-                '''
-            }
-        }
-
-        stage('Build') {
-            steps {
-                echo 'This is a minimal pipeline'
-            }
+    stage('Build') {
+        def builds = [:]
+        builds['seed'] = {
+            sh 'cd seed'
+            // assumes you have the sbt plugin installed and created an sbt installation named 'sbt-0.13.13'
+            sh "${tool name: 'sbt-0.13.13', type: 'org.jvnet.hudson.plugins.SbtPluginBuilder$SbtInstallation'}/bin/sbt compile test"
         }
     }
 }
