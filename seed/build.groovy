e67d590b6919a1c10ebeada969911210af7fe5f5
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-03-15 11:07:00 -0400

    Add sbt build

--- seed/build.groovy
+++ seed/build.groovy
@@ -1,3 +1,21 @@
 node {
-    echo 'The pipeline started'
+    def branchVersion = ""
+
+    stage('Checkout') {
+       // checkout repository\
+        checkout scm
+
+        sh "git checkout ${caller.env.BRANCH_NAME}"
+    }
+
+    stage('Determine Branch Version') {
+        // add sbt to path
+        env.PATH = "${tool 'sbt'}/bin:${env.PATH}"
+
+        branchVersion = env.BRANCH_NAME
+    }
+
+    stage('Package project') {
+        sh 'sbt clean test assembly'
+    }
 }
