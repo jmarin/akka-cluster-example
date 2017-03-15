a0b5ae9b952cfea1f7a3682f839fa694f19183ae
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-03-15 12:06:18 -0400

    Add steps

--- build.groovy
+++ build.groovy
@@ -4,8 +4,7 @@ node {
     }
 
     stage('Build') {
-        def builds = [:]
-        builds['scala'] = {
+        steps {
             // assumes you have the sbt plugin installed and created an sbt installation named 'sbt-0.13.13'
             sh "${tool name: 'sbt', type: 'org.jvnet.hudson.plugins.SbtPluginBuilder$SbtInstallation'}/bin/sbt compile test"
         }
