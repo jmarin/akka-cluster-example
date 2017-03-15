229b978200c62ea92afa1357974987ed3c5e0384
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-03-15 11:16:31 -0400

    Add sbt build

--- seed/build.groovy
+++ seed/build.groovy
@@ -8,14 +8,13 @@ node {
         sh "git checkout ${env.BRANCH_NAME}"
     }
 
-    stage('Determine Branch Version') {
-        // add sbt to path
-        env.PATH = "${tool 'sbt'}/bin:${env.PATH}"
+   stage('Build') {
+       def builds = [:]
+       builds['seed'] = {
+           // assumes you have the sbt plugin installed and created an sbt installation named 'sbt-0.13.13'
+           sh "${tool name: 'sbt-0.13.13', type: 'org.jvnet.hudson.plugins.SbtPluginBuilder$SbtInstallation'}/bin/sbt clean test assembly"
+       }
+   }
 
-        branchVersion = env.BRANCH_NAME
-    }
 
-    stage('Package project') {
-        sh 'sbt clean test assembly'
-    }
 }
