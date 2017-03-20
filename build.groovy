0b6d24a38872b28159afbff7162db1e404360255
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-03-20 12:51:18 -0400

    Go into relative paths for building docker images

--- build.groovy
+++ build.groovy
@@ -10,9 +10,9 @@ node('') {
     stage('Scala Build') {
         // assumes you have the sbt plugin installed and created an sbt installation named 'sbt-0.13.1'
         sh "${tool name: 'sbt 0.13.1', type: 'org.jvnet.hudson.plugins.SbtPluginBuilder$SbtInstallation'}/bin/sbt clean assembly"
-        sh "cp seed/target/scala-2.12/seed.jar target/scala-2.12/."
-        sh "cp frontend/target/scala-2.12/frontend.jar target/scala-2.12/."
-        sh "cp backend/target/scala-2.12/backend.jar target/scala-2.12/."
+        //sh "cp seed/target/scala-2.12/seed.jar target/scala-2.12/."
+        //sh "cp frontend/target/scala-2.12/frontend.jar target/scala-2.12/."
+        //sh "cp backend/target/scala-2.12/backend.jar target/scala-2.12/."
     }
 
     docker.withRegistry('', 'dtr') {
@@ -25,15 +25,21 @@ node('') {
           sh "git rev-parse HEAD > .git/commit-id"
           def commit_id = readFile('.git/commit-id').trim()
 
-          def seedImage = docker.build("${owner}/${project_root_name}-${seedName}:${commit_id}", "./${seedName}")
+          sh "cd seed"
+
+          def seedImage = docker.build("${owner}/${project_root_name}-${seedName}:${commit_id}", ".")
           seedImage.push(commit_id)
           seedImage.push('latest')
 
-          def frontendImage = docker.build("${owner}/${project_root_name}-${frontendName}:${commit_id}", "./${frontendName}")
+          sh "cd ../frontend"
+
+          def frontendImage = docker.build("${owner}/${project_root_name}-${frontendName}:${commit_id}", ".")
           frontendImage.push(commit_id)
           frontendImage.push('latest')
 
-          def backendImage = docker.build("${owner}/${project_root_name}-${backendName}:${commit_id}", "./${backendName}")
+          sh "cd ../backend"
+
+          def backendImage = docker.build("${owner}/${project_root_name}-${backendName}:${commit_id}", ".")
           backendImage.push(commit_id)
           backendImage.push('latest')
       }
