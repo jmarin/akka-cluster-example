a6b86d2a7d55f18cebd00f25695012c92ea1a803
Juan Marin Otero <juan.marin.otero@gmail.com>
2017-03-20 13:21:32 -0400

    Update jar names

--- build.groovy
+++ build.groovy
@@ -14,9 +14,9 @@ node('') {
 
     docker.withRegistry('', 'dtr') {
       stage('Docker Build') {
-          sh "cp seed/target/scala-2.12/seed.jar target/scala-2.12/."
-          sh "cp frontend/target/scala-2.12/frontend.jar target/scala-2.12/."
-          sh "cp backend/target/scala-2.12/backend.jar target/scala-2.12/."
+          sh "cp seed/target/scala-2.12/seed.jar target/scala-2.12/seed.jar"
+          sh "cp frontend/target/scala-2.12/frontend.jar target/scala-2.12/frontend.jar"
+          sh "cp backend/target/scala-2.12/backend.jar target/scala-2.12/backend.jar"
 
           def seedName = "seed"
           def frontendName = "frontend"
@@ -25,15 +25,15 @@ node('') {
           sh "git rev-parse HEAD > .git/commit-id"
           def commit_id = readFile('.git/commit-id').trim()
 
-          def seedImage = docker.build("${owner}/${project_root_name}-${seedName}:${commit_id}", "${seedName}")
+          def seedImage = docker.build("${owner}/${project_root_name}-${seedName}:${commit_id}", "${seedName}/.")
           seedImage.push(commit_id)
           seedImage.push('latest')
 
-          def frontendImage = docker.build("${owner}/${project_root_name}-${frontendName}:${commit_id}", "${frontendName}")
+          def frontendImage = docker.build("${owner}/${project_root_name}-${frontendName}:${commit_id}", "${frontendName}/.")
           frontendImage.push(commit_id)
           frontendImage.push('latest')
 
-          def backendImage = docker.build("${owner}/${project_root_name}-${backendName}:${commit_id}", "${backendName}")
+          def backendImage = docker.build("${owner}/${project_root_name}-${backendName}:${commit_id}", "${backendName}/.")
           backendImage.push(commit_id)
           backendImage.push('latest')
       }
